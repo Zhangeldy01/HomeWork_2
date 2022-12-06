fd66988656e7568ceaaab16bd04ed1774b5ea194
Zhangeldy <shakhanov_zhan@mail.ru>
2022-12-06 10:33:40 +0300

    added .gitignore

---
+++ HomeWork_2_Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine ("Enter a number: ");
+ int dayofweek = Convert.ToInt32(Console.ReadLine());
+if (dayofweek == 6 || dayofweek == 7) 
+Console.WriteLine ("day of rest");
+ else if (dayofweek <= 5) 
+ Console.WriteLine ("working day");
+ else if (dayofweek > 7) Console.WriteLine ("Error: There are only 7 days in the week!");
+ 
