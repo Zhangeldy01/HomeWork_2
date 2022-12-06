fc7e3341e12d0b6fb001ff52300d575919693b95
Zhangeldy <shakhanov_zhan@mail.ru>
2022-12-06 10:28:30 +0300

    added HomeWork_2_Task10

---
+++ HomeWork_2_Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8 
+918 -> 1 */
+
+int GetRandomNumber()
+    {
+      return new Random().Next(100, 1000);
+    }
+void ShowRandomNumber (int number)
+    {
+    Console.WriteLine($"Случайное трехзначное число: {number}");
+    int result = ((number / 10) % 10);
+    Console.WriteLine($"Вторая цифра: {result}");
+    }
+ShowRandomNumber(GetRandomNumber());
